108feade955da3530c3b66875e68531e12a88b09
kush Muhamad <kushmohammed98@yahoo.com>
2025-04-29 15:05:35 +0300

    Merge backend changes from Kush

--- backend/src/main/java/com/kush/nada/services/AuthService.java
+++ backend/src/main/java/com/kush/nada/services/AuthService.java
@@ -1,6 +1,7 @@
 package com.kush.nada.services;
 
 import com.kush.nada.enums.Role;
+import com.kush.nada.exceptions.BadRequestException;
 import com.kush.nada.models.UserEntity;
 import com.kush.nada.repositories.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,12 +20,31 @@ public class AuthService {
 
 
     public UserEntity registerUser(UserEntity user){
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        if (userRepository.count() == 0) {
-            user.setRole(Role.ADMIN);
-        } else {
-            user.setRole(Role.USER);
+        if (user.getName() == null || user.getName().trim().isEmpty()) {
+            throw new BadRequestException("Name cannot be empty");
         }
+        if (user.getEmail() == null || user.getEmail().trim().isEmpty()) {
+            throw new BadRequestException("Email cannot be empty");
+        }
+        if (user.getPassword() == null || user.getPassword().length() < 6) {
+            throw new BadRequestException("Password must be at least 6 characters long");
+        }
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new BadRequestException("Email is already in use");
+        }
+        if (userRepository.existsByName(user.getName())) {
+            throw new BadRequestException("Name is already taken");
+        }
+        if (user.getAddress() == null || user.getAddress().isEmpty()) {
+            throw new BadRequestException("Address is required.");
+        }
+        if (user.getPhoneNumber() == null || user.getPhoneNumber().isEmpty()) {
+            throw new BadRequestException("Phone Number is required.");
+        }
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRole(userRepository.count() == 0 ? Role.ADMIN : Role.USER);
+
+
         return userRepository.save(user);
     }
 
--- backend/src/main/java/com/kush/nada/services/JwtService.java
+++ backend/src/main/java/com/kush/nada/services/JwtService.java
@@ -22,7 +22,7 @@ public class JwtService {
     // Get the Key object from the SECRET
     private Key getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET);// Decode the Base64 secret
-        System.out.println(keyBytes); // [B@1f26fdd
+        //System.out.println(keyBytes); // [B@1f26fdd
         return Keys.hmacShaKeyFor(keyBytes); // Generate the key
     }
 
