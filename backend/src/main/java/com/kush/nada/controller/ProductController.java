45e0c2470b7210034881a2b249858305707ab2c6
704KC <nakibonekamarymargret@gmail.com>
2025-05-01 12:41:39 +0300

    Merged changes in Bakaluba into Mary

--- backend/src/main/java/com/kush/nada/controller/ProductController.java
+++ backend/src/main/java/com/kush/nada/controller/ProductController.java
@@ -14,18 +14,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.IOException;
 import java.util.List;
@@ -156,7 +144,7 @@ import java.util.Map;
 //        return responseService.createResponse(200, "Product deleted", request, HttpStatus.OK);
 //    }
 //}
-@CrossOrigin
+
 @RestController
 @RequestMapping("/product")
 public class ProductController {
